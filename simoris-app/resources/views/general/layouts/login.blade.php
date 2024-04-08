b2f77c5b1f79a63761b350f5111f9eabfbfc672f
fireeemaan <firmancakep222@gmail.com>
2024-04-09 00:24:50 +0700

    feat: general layout

--- simoris-app/resources/views/general/layouts/login.blade.php
+++ simoris-app/resources/views/general/layouts/login.blade.php
@@ -1,7 +1,7 @@
 @extends('general.layouts.main')
 
 @section('content')
-  <div class="formcontainer h-[90vh] w-screen flex flex-col justify-center items-center">
+  <div class="formcontainer h-[100dvh] w-screen flex flex-row justify-center items-center bg-[#DDF2FD] gap-48">
     @if($errors->any())
       <div class="alert">
         <ul>
@@ -21,15 +21,31 @@
         {{ session('update') }}
       </div>
     @endif
-    <form action="" method="POST" class="flex flex-col gap-5">
-      @csrf
-      <input type="email" class="@error('email') is-invalid @enderror" name="email" placeholder="example@mail.com" value="{{ old('email') }}" autofocus required>
-      @error('email')
-      <div class="invalid-feedback">{{ $message }}</div>
-      @enderror
-      <input type="password" class="" name="password" placeholder="*********" required>
-      <button type="submit" class="p-2 bg-black text-white">Login</button>
-    </form>
-    <a href="/register" class="mt-4">REGISTER</a>
+
+    <div class="flex flex-col w-[864px] inset-x-7">
+      <h1 class="text-7xl font-bold tracking-widest text-[#164863]">SIMORIS</h1>
+      <h2 class="text-4xl font-semibold text-[#164863]">Sistem Monitoring Distribusi Inseminasi Buatan</h2>
+      <p class="text-2xl font-light mt-3 text-[#427D9D]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer elementum purus quis ultricies luctus. Quisque a risus urna. Cras eget ullamcorper massa, a laoreet enim.</p>
+    </div>
+
+    <div class="flex flex-col justify-center items-center size-[63dvh] bg-white rounded-3xl shadow-lg">
+      <h1 class="text-4xl font-semibold text-[#427D9D] mb-16">Selamat Datang<span class="text-[#164863]">.</span></h1>
+      <form action="" method="POST" class="flex flex-col gap-y-9">
+        @csrf
+        <input type="email" class="@error('email') is-invalid @enderror w-96 h-14 rounded-xl" name="email" placeholder="example@mail.com" value="{{ old('email') }}" autofocus required>
+        @error('email')
+        <div class="invalid-feedback">{{ $message }}</div>
+        @enderror
+        <input type="password" class="w-96 h-14 rounded-xl" name="password" placeholder="*********" required>
+        <a class="text-base font-medium text-black/50 hover:text-black/65 right-0 text-right -mt-7">Lupa Password?</a>
+        <button type="submit" class="p-2 bg-[#427D9D] hover:bg-[#33617a] ease-in-out duration-100 text-white text-2xl font-semibold w-96 h-14 rounded-xl">Masuk</button>
+      </form>
+      <div class="flex flex-col justify-center items-center mt-8">
+        <p class="text-black/60 text-lg font-medium">Belum punya akun?</p>
+        <a href="/register" class="underline text-lg font-semibold text-[#164863] hover:text-[#5c7281] ease-in-out duration-100">Daftar Sekarang</a>
+      </div>
+      
+    </div>
+
   </div>
 @endsection
--- simoris-app/resources/views/general/partials/navbar.blade.php
+++ simoris-app/resources/views/general/partials/navbar.blade.php
@@ -1,9 +1,9 @@
-<nav class="flex flex-row justify-center items-center h-[10vh] w-screen">
-  <p class="font-extrabold text-xl absolute left-32">Simoris</p>
-  <ul class="flex flex-row gap-10">
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-      <li>Halo</li>
+<nav class="flex flex-row justify-left items-center h-14 w-screen shadow-2xl">
+  <img src="{{asset('assets/logo.svg')}}" alt="Simoris" class="ml-10 mr-40 h-10">
+  <!-- <p class="font-extrabold text-xl absolute left-32">Simoris</p> -->
+  <ul class="flex flex-row gap-x-16 text-lg font-semibold">
+      <li class="text-[#164863] font-bold">Beranda</li>
+      <li class="text-black/40">Tentang Kami</li>
+      <li class="text-black/40">Layanan Kami</li>
   </ul>
 </nav>
