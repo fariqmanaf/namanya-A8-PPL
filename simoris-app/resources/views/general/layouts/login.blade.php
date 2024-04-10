6bf756ede99f6bfe184570e8da9dfc9d1a88b886
fireeemaan <firmancakep222@gmail.com>
2024-04-10 13:02:44 +0700

    feat: general role choose responsive layout

--- simoris-app/resources/views/general/layouts/login.blade.php
+++ simoris-app/resources/views/general/layouts/login.blade.php
@@ -22,13 +22,15 @@
       </div>
     @endif
 
-    <div class="flex flex-col w-[40rem] 2xl:w-[864px] inset-x-7">
+    <img src="{{asset("assets/bg-hero.png")}}" class="absolute z-0 w-full top-0" alt="bg-hero">
+
+    <div class="z-10 flex flex-col w-[40rem] 2xl:w-[864px] inset-x-7">
       <h1 class="text-6xl 2xl:text-7xl font-bold tracking-widest text-[#164863] -mb-2">SIMORIS</h1>
       <h2 class="text-2xl 2xl:text-4xl font-medium tracking-tighter text-[#164863]">Sistem Monitoring Distribusi Inseminasi Buatan</h2>
       <p class="text-base 2xl:text-2xl font-light mt-2 2xl:mt-4 text-[#427D9D]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer elementum purus quis ultricies luctus. Quisque a risus urna. Cras eget ullamcorper massa, a laoreet enim.</p>
     </div>
 
-    <div class="flex flex-col justify-center items-center size-[27rem] 2xl:size-[38rem] bg-white rounded-2xl 2xl:rounded-3xl shadow-lg">
+    <div class="z-10 flex flex-col justify-center items-center size-[27rem] 2xl:size-[38rem] bg-white rounded-2xl 2xl:rounded-3xl shadow-lg">
       <h1 class="text-2xl 2xl:text-4xl font-semibold text-[#427D9D] mb-11">Selamat Datang<span class="text-[#164863]">.</span></h1>
       <form action="" method="POST" class="flex flex-col gap-y-9">
         @csrf
@@ -38,7 +40,7 @@
         @enderror
         <input type="password" class="w-80 h-11 rounded-lg 2xl:w-96 2xl:h-14 2xl:rounded-xl" name="password" placeholder="*********" required>
         <a href="" class="text-xs 2xl:text-base font-medium text-black/50 hover:text-black/65 right-0 text-right -mt-7">Lupa Password?</a>
-        <button type="submit" class="login-btn">Masuk</button>
+        <button type="submit" class="btn-login">Masuk</button>
       </form>
       <div class="flex flex-col justify-center items-center mt-8">
         <p class="text-black/60 text-xs 2xl:text-lg font-medium">Belum punya akun?</p>
--- simoris-app/resources/views/general/layouts/register.blade.php
+++ simoris-app/resources/views/general/layouts/register.blade.php
@@ -1,12 +1,25 @@
 @extends('general.layouts.main')
 
 @section('content')
-  <div class="register formcontainer h-[100dvh] w-screen flex flex-col gap-10 justify-center items-center bg-slate-400">
-    <div class="flex flex-col justify-center items-center size-[27rem] 2xl:size-[38rem] bg-white shadow-lg rounded-2xl 2xl:rounded-3xl">
-      <!-- <p>Ingin Daftar Sebagai Apa?</p> -->
-      <div class="btn-choose-role">
-        <a href="/register/mantri/step-1" class="p-2 bg-black text-white">Daftar Sebagai Mantri</a>
-        <a href="/register/peternak/step-1" class="p-2 bg-black text-white">Daftar Sebagai Peternak</a>
+  <div class="register formcontainer h-[100dvh] w-screen flex flex-col gap-10 justify-center items-center bg-[#DDF2FD]">
+    <img src="{{asset("assets/bg.png")}}" class="z-0 absolute top-0 mt-7 w-full" alt="">
+    <div class="z-10 flex flex-col justify-center items-center size-[27rem] 2xl:size-[38rem] bg-white shadow-lg rounded-2xl 2xl:rounded-3xl">
+      <div class="mt-16 2xl:mt-24">
+        <p class="text-[#427D9D] text-2xl 2xl:text-3xl font-semibold text-center">Ingin Daftar Sebagai Apa?</p>
+        <div class="flex gap-11 mt-5 2xl:mt-7">
+          <a href="/register/peternak/step-1" class="btn-choose-role">
+            <img class="h-14 2xl:h-20" src="{{asset("assets/icon/farmer-icon.svg") }}" alt="">
+            Peternak
+          </a>
+          <a href="/register/mantri/step-1" class="btn-choose-role">
+          <img class="h-14 2xl:h-20" src="{{asset("assets/icon/mantri-icon.svg") }}" alt="">
+            Mantri
+          </a>
+        </div>
+        <p class="flex flex-col text-sm 2xl:text-lg font-medium text-black/45 text-center mt-16 2xl:mt-24">
+          Sudah punya akun? 
+          <a href="/" class="underline text-[#164863] hover:text-[#5c7281] font-semibold">Masuk</a>
+        </p>
       </div>
     </div>
   </div>
